dafb0f470ef8520b07bacb2cd90cbbceb0117b41
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2025-10-31 21:03:06 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:dev/modules/gitignore.nix

---
+++ dev/modules/gitignore.nix
@@ -0,0 +1,14 @@
+{ lib, ... }:
+{
+  perSystem =
+    psArgs@{ pkgs, ... }:
+    {
+      options.gitignore = lib.mkOption {
+        type = lib.types.listOf lib.types.singleLineStr;
+      };
+      config.files.files = lib.singleton {
+        path_ = ".gitignore";
+        drv = psArgs.config.gitignore |> lib.naturalSort |> lib.concatLines |> pkgs.writeText ".gitignore";
+      };
+    };
+}
